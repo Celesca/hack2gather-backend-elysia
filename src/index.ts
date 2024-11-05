43d82f5c7f77fff87c5a60151d0b52a1ee2995e9
celesca <folk.sawit@gmail.com>
2024-11-05 13:30:40 +0700

    add: swagger openAPI

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
 import { swipeController } from "./controllers/swipeController";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").use(swipeController).listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").use(swagger()).use(swipeController).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
