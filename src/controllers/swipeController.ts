200ce4ff313c920bd780b9f5ea49b30431af96a2
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-11-07 18:43:50 +0700

    add: params and validation

--- src/controllers/swipeController.ts
+++ src/controllers/swipeController.ts
@@ -1,4 +1,13 @@
-import Elysia from "elysia";
+import { Elysia, t } from "elysia";
 
-export const swipeController = new Elysia({ prefix: "/swipe" })
-   .get("/", ({}) => {})
+export const swipeController = new Elysia({ prefix: "/swipe" }).get(
+  "/:index",
+  ({ params: { index } }) => {
+    return index;
+  },
+  {
+   params: t.Object({
+      index: t.Number()
+   })
+  }
+);
