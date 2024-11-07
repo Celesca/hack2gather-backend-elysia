f6e456a4526c2e8e2bd01204c6bd8d44fd9ed93b
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-11-07 18:41:19 +0700

    format code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { Elysia } from "elysia";
 import { swipeController } from "./controllers/swipeController";
 import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").use(swagger()).use(swipeController).listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .use(swagger())
+  .use(swipeController)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
