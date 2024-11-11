cf8ea14ab4edd0e014fc020c762be7554e9561ab
celesca <folk.sawit@gmail.com>
2024-11-11 19:30:35 +0700

    removed the start endpoint in the messageController

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -3,45 +3,6 @@ import { prisma } from "../prisma"; // Prisma client
 
 export const messageController = new Elysia({ prefix: "/message" })
 
-  .post(
-
-    "/start",
-    async ({ body, error }) => {
-      const { senderID, receiverID, messageContent } = body;
-
-      // Check if both users exist
-      const sender = await prisma.user.findUnique({
-        where: { UserID: senderID },
-      });
-
-      const receiver = await prisma.user.findUnique({
-        where: { UserID: receiverID },
-      });
-
-      if (!sender || !receiver) {
-        return error(404, "Sender or receiver not found");
-      }
-
-      // Create the initial message
-      const newMessage = await prisma.message.create({
-        data: {
-          SenderID: senderID,
-          ReceiverID: receiverID,
-          MessageContent: messageContent,
-        },
-      });
-
-      return newMessage;
-    },
-    {
-      body: t.Object({
-        senderID: t.String(),
-        receiverID: t.String(),
-        messageContent: t.String(),
-      }),
-    }
-  )
-
   // Send a message in an existing thread
   .post(
     "/send",
