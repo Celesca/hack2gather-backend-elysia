1233717a9326d5a6b495e59040a3e2bea5d3cfe5
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-11-23 22:52:02 +0700

    add: post /rating

--- src/controllers/ratingController.ts
+++ src/controllers/ratingController.ts
@@ -2,3 +2,44 @@ import { Elysia, t } from "elysia";
 import { prisma } from "../prisma"; // Prisma client
 
 export const ratingController = new Elysia({ prefix: "/rating" })
+
+// Rating to someone
+ratingController.post("/", async ({ body, error }) => {
+    const { ratedByID, ratedUserID, ratingValue, comment } = body;
+
+    // Check if the user exists
+    const ratedBy = await prisma.user.findUnique({
+        where: { UserID: ratedByID },
+    });
+
+    if (!ratedBy) {
+        return error(404, "User not found");
+    }
+
+    // Check if the user exists
+    const ratedUser = await prisma.user.findUnique({
+        where: { UserID: ratedUserID },
+    });
+
+    if (!ratedUser) {
+        return error(404, "User not found");
+    }
+
+    // Create the new rating
+    const newRating = await prisma.userRating.create({
+        data: {
+            RatedByID: ratedByID,
+            RatedUserID: ratedUserID,
+            RatingValue: ratingValue,
+            Comment: comment,
+        },
+    });
+
+}, {
+    body: t.Object({
+        ratedByID: t.String(),
+        ratedUserID: t.String(),
+        ratingValue: t.Number(),
+        comment: t.Optional(t.String()),
+    }),
+});
