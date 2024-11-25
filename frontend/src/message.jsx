97aef869f0d3003e9f2f63af4fdc7ada70a2a6a9
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-11-26 00:10:28 +0700

    fetchChats

--- frontend/src/message.jsx
+++ frontend/src/message.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ChatList from './components/ChatList';
 import ChatBox from './components/ChatBox';
+import axios from 'axios';
 
 const Message = () => {
   const [activeChat, setActiveChat] = useState(null);
+  const [userID, setUserID] = useState(null);
 
   const contacts = [
     { id: 1, name: 'John Doe', message: 'Hey! How are you?' },
     { id: 2, name: 'Jane Smith', message: 'Let’s catch up soon!' },
     { id: 3, name: 'Mark Taylor', message: 'Check this out.' },
   ];
+
+  const fetchChats = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/message/inbox/${userID}`);
+      console.log(response.data[0]);
+      setActiveChat(response.data[0]);
+    } catch (error) {
+      console.error('Error fetching chats:', error);
+    }
+  };
+
+  fetchChats();
+
+  useEffect(() => {
+    const user = localStorage.getItem('UserID');
+    if (user) {
+      setUserID(user);
+      console.log(user);
+    }
+  }, [])
+
+
 
   const handleChatSelect = (contact) => {
     setActiveChat(contact);
